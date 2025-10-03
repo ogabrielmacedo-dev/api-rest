8b621dbef537d8236c924e90072368c9ef790c7c
Gabriel de Macedo <ogabrielmacedo.dev@gmail.com>
2025-10-03 19:59:49 -0300

    API-REST finalizada

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/app-error";
+import { z } from "zod";
 
 class ProductsController {
 	/**
@@ -16,14 +18,25 @@ class ProductsController {
 	}
 
 	create(request: Request, response: Response) {
-		const { name, price } = request.body;
 		// response.send(`Produto: ${name} | Valor: ${price}`);
 
+		const bodySchema = z.object({
+			name: z
+				.string({ required_error: "Name is required" })
+				.trim()
+				.min(6, { message: "Name must be 6 or more characters" }),
+			price: z.number({ required_error: "Price is required" }).positive({ message: "Price must be positive" }),
+			// nullish() serve para deixar o campo opcional na requisição
+		});
+
+		const { name, price } = bodySchema.parse(request.body);
+
 		if (!name || !price) {
-			return response.status(400).json({ error: 'Os campos "name" e "price" são obrigatórios' });
+			throw new AppError("O nome e preço do produto são obrigatórios");
 		}
 
-		throw new Error("ERRO DE EXEMPLO!");
+		// throw new Error("Erro ao tentar criar um produto");
+		// throw new AppError("Erro ao tentar criar um produto");
 
 		// Passando a resposta em JSON
 		return response.status(201).json({ name, price, user_id: request.user_id });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
+import { AppError } from "./utils/app-error";
+import { ZodError } from "zod";
 
 // Colocar a porta em uma const facilita caso precise trocar a porta
 const PORT = 3333;
@@ -10,8 +12,24 @@ app.use(express.json());
 
 app.use(routes);
 
+/**
+ * ERRO DO CLIENTE
+ * 400 (Bad Request): Erro do cliente
+ *
+ * ERRO DO SERVIDOR
+ * 500 (Internal Server Error): Erro interno do servidor
+ */
+
 app.use((error: any, request: Request, response: Response, next: NextFunction) => {
-	response.status(500).json({ message: error.message });
+	if (error instanceof AppError) {
+		return response.status(error.statusCode).json({ message: error.message });
+	}
+
+	if (error instanceof ZodError) {
+		return response.status(400).json({ message: "Validation error!", issues: error.format() });
+	}
+
+	return response.status(500).json({ message: error.message });
 });
 
 // É possível passar uma função de callback
